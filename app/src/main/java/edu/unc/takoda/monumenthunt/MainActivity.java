50198b93ab2ac7554738ddcfc552c38a1228eaab
takoda1 <31773273+takoda1@users.noreply.github.com>
2017-10-30 14:34:23 -0400

    Initial commit

---
+++ app/src/main/java/edu/unc/takoda/monumenthunt/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.unc.takoda.monumenthunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
