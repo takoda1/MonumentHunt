253315732df30305cc655cfd9d056920b17404fb
jtizon001 <tizon.jonathan@gmail.com>
2017-12-04 01:17:44 -0500

    mon and databases

--- app/src/main/java/edu/unc/takoda/monumenthunt/MonumentData.java
+++ app/src/main/java/edu/unc/takoda/monumenthunt/MonumentData.java
@@ -17,6 +17,24 @@ public class MonumentData {
 
     public MonumentData(){
         monuments.add(new Monument("Old Well", 35.91205448, -79.05123889, R.drawable.oldwell));
+        monuments.add(new Monument("Old East", 35.9123, -79.0508, R.drawable.oldeast));
+        monuments.add(new Monument("Davie Poplar", 35.9129, -79.0517, R.drawable.daviepoplar));
+        monuments.add(new Monument("Carolina Inn", 35.9099, -79.0546, R.drawable.carolinainn));
+        monuments.add(new Monument("Charlie Choo Choo Justice", 35.907337, -79.049395, R.drawable.choochoo));
+        monuments.add(new Monument("9/11 Memorial", 35.907530, -79.046390, R.drawable.nineeleven));
+        monuments.add(new Monument("Bell Tower", 35.9086, -79.0492, R.drawable.belltower));
+        monuments.add(new Monument("Thomas Wolfe Memorial", 35.910650,-79.049410,  R.drawable.wolfe));
+        monuments.add(new Monument("Eve Carson Memorial Gardens", 35.910750, -79.0505480, R.drawable.eveCar));
+        monuments.add(new Monument("Carolina Alumni in Memory of those Lost in Military Service",
+                35.911070, -79.052380, R.drawable.vetMemorial));
+        monuments.add(new Monument("Playmakers Theatre", 35.9120, -79.0503, R.drawable.playmakers));
+        monuments.add(new Monument("Caldwell Monument", 35.913290, -79.052200, R.drawable.oblisk));
+        monuments.add(new Monument("Unsung Founders Memorial", 35.913620, -79.052120, R.drawable.unsung));
+        monuments.add(new Monument("Susan Williams Graham Memorial Fountain", 35.914230,
+                -79.049770, R.drawable.fountain));
+        monuments.add(new Monument("Memorial to Founding Trustees", 35.912540 , -79.052480, R.drawable.founders));
+
+
         /*for(int i = 0; i < names.length; i++){
             monuments.add(new Monument(names[i], latitudes[i], longitutdes[i], drawableIDs[i]));
         }*/
--- app/src/main/java/edu/unc/takoda/monumenthunt/StatsDatabase.java
+++ app/src/main/java/edu/unc/takoda/monumenthunt/StatsDatabase.java
@@ -1,12 +1,68 @@
 package edu.unc.takoda.monumenthunt;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 
 /**
  * Created by takoda on 11/16/2017.
  */
 
 public class StatsDatabase {
-    private SQLiteDatabase db;
+    private static SQLiteDatabase db;
+    private static StatsDatabase db1;
+
+
+    public StatsDatabase(){
+        db=SQLiteDatabase.openOrCreateDatabase("/sdcard/db",null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS SinglePlayerScore (MonumentID INT PRIMARY KEY, date DATE)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS SinglePlayerTimedScore (MonumentID INT, date DATE)");
+        //multiplayer table
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Monuments(ID INT PRIMARY KEY, Name STRING, Lat FLOAT, Long FLOAT)");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS GameEvents(ID INT PRIMARY KEY, PlayerID INT, TimeOfArrival datetime)");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Player(ID INT PRIMARY KEY)");
+
+        MonumentData x=new MonumentData();
+
+        ArrayList<Monument> monList = x.monumentList(); //new ArrayList<MonumentData>();
+
+
+
+        for (int i=0;i< monList.size();i++ ){
+            ContentValues row= new ContentValues();
+            row.put("Name",monList.get(i).getName());
+            row.put("Lat",monList.get(i).getLatitude());
+            row.put("Long", monList.get(i).getLongitude());
+
+            db.insert("Monuments",null,row);
+
+            //db.execSQL("INSERT INTO Monuments (Name, Lat, Long)");
+        }
+        //assuming you have a list of monuments found in single
+
+
+    }
+    public static void init (){
+        if(db1==null) {
+
+            db1 = new StatsDatabase();
+
+
+        }
+
+    }
+    public static StatsDatabase getInstance(){
+        return db1;
+    }
+
+
+
+
+    //db= openOrCreateDatabase("MyDatabase", Context.MODE_PRIVATE,null);
 
 }
